44ab227380b89e111ac3f333b48e3f2380c897f2
Adel <adel.elia@gmail.com>
2018-02-12 16:38:23 +0100

    'dwarfs standing on the shoulders of giants' puzzle

---
+++ src/main/java/puzzles/dwarfs/Solution.java
@@ -0,0 +1,49 @@
+package puzzles.dwarfs;
+
+import java.util.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String[] args) {
+        HashSet<Integer> roots = new HashSet<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // the number of relationships of influence
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt(); // a relationship of influence between two people (x xChildren y)
+            int y = in.nextInt();
+            if (roots.contains(y)) {
+                roots.remove(y);
+            }
+            roots.add(x);
+            List<Integer> xChildren = graph.getOrDefault(x, new ArrayList<>());
+            xChildren.add(y);
+            graph.put(x, xChildren);
+        }
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        int max = roots.stream()
+                .map(r -> dps(r, graph, memo))
+                .max(Integer::compareTo)
+                .orElseThrow(RuntimeException::new);
+        System.out.println(max);
+    }
+
+    protected static int dps(int root, HashMap<Integer, List<Integer>> graph, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(root)) {
+            return memo.get(root);
+        } else if (graph.get(root) == null) {
+            return 1;
+        } else {
+            int max = 1 + graph.get(root).stream()
+                    .map(e -> dps(e, graph, memo))
+                    .max(Integer::compareTo).orElse(0);
+            memo.put(root, max);
+            return max;
+        }
+    }
+}
---
+++ src/test/java/puzzles/dwarfs/SolutionTest.java
@@ -0,0 +1,47 @@
+package puzzles.dwarfs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test void shouldReturnCorrectNumberOfRelationsForTwoElements() {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, Arrays.asList(2));
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        assertEquals(2, Solution.dps(1, graph, memo));
+    }
+    @Test void shouldReturnCorrectDpsForDepthOf3() {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, Arrays.asList(2));
+        graph.put(2, Arrays.asList(3));
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        assertEquals(3, Solution.dps(1, graph, memo));
+    }
+    @Test void shouldReturnCorrectDpsForDepthOf5() {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, Arrays.asList(2));
+        graph.put(2, Arrays.asList(3));
+        graph.put(3, Arrays.asList(4));
+        graph.put(4, Arrays.asList(5));
+
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        assertEquals(5, Solution.dps(1, graph, memo));
+    }
+    @Test void shouldReturnCorrectDpsForMultipleChildrenPerParent() {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, Arrays.asList(2,3));
+        graph.put(2, Arrays.asList(3,5,6));
+        graph.put(3, Arrays.asList(4,7,8));
+        graph.put(8, Arrays.asList(10));
+
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        assertEquals(5, Solution.dps(1, graph, memo));
+    }
+}
